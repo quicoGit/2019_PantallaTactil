5491045d73f5ae139e1b150918af4a993fccde5d
Quico Orozco <esi@plumbaria.com>
2019-01-10 18:50:21 +0100

    Commit terminado Ejercicio 5.8 Uso de la pantalla táctil

--- app/src/main/java/com/plumbaria/e_5_8_pantallatactil/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_5_8_pantallatactil/MainActivity.kt
@@ -2,11 +2,23 @@ package com.plumbaria.e_5_8_pantallatactil
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnTouchListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val entrada:TextView = findViewById(R.id.textViewEntrada)
+        entrada.setOnTouchListener(this)
     }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        val salida:TextView = findViewById(R.id.textViewSalida)
+        salida.append("\n " + event.toString() + "\n  Presión:" + event?.pressure + "\n Tamaño:" + event?.size + "\n")
+        return true
+    }
+
+
 }
