ecdc5596f3bada897fdb1767823c9765ec2b3eaa
Quico Orozco <esi@plumbaria.com>
2019-01-11 18:49:07 +0100

    Commit terminado Ejercicio 5.9. Pantalla Multitouch

--- app/src/main/java/com/plumbaria/e_5_8_pantallatactil/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_5_8_pantallatactil/MainActivity.kt
@@ -16,7 +16,20 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
 
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
         val salida:TextView = findViewById(R.id.textViewSalida)
-        salida.append("\n " + event.toString() + "\n  Presión:" + event?.pressure + "\n Tamaño:" + event?.size + "\n")
+
+        var acciones:Array<String> = arrayOf("ACTION_DOWN","ACTION_UP","ACTION_MOVE",
+            "ACTION_CANCEL", "ACTION_OUTSIDE", "ACTION_POINTER_DOWN", "ACTION_POINTER_UP")
+        var accion:Int? = event?.action
+        var codigoAccion:Int? = accion?.and(MotionEvent.ACTION_MASK)
+        var iPuntero = (accion?.and(MotionEvent.ACTION_POINTER_INDEX_MASK))?.shr(MotionEvent.ACTION_POINTER_INDEX_SHIFT)
+        salida.append("\n" + acciones[codigoAccion!!])
+        salida.append(" Generada por puntero: " + iPuntero)
+        for (i in 0..(event?.pointerCount!!-1)){
+            salida.append("\npuntero:" + event?.getPointerId(i) +
+                    " x:" + event.getX(i) + " y:" + event.getY(i)
+            )
+        }
+        salida.append("\n")
         return true
     }
 
